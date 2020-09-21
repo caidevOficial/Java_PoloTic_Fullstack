db920e0d1a115f8ed52c8b144d1afe671ae9248e
caidev <caidev@caidev-Deepin>
2020-09-20 22:22:58 -0300

    Calculadora Funcional

---
+++ Calculadora/src/Logica/CalcuSuma.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package Logica;
+import GUI.Front;
+
+/**
+ * @author caidev
+ *
+ */
+public class CalcuSuma {
+
+	public static void Sumar(String num1, String num2) {
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 //se instancia el objeto.
+        Front ventana = new Front();
+        ventana.setVisible(true); // la hago visible
+        //para que aparezca en el medio, se pone relative null
+        // ya que no toma nada como referencia para tener uan locacion.
+        ventana.setLocationRelativeTo(null);
+
+	}
+
+}
