994d2cb62570dbcd9e2b78d8fecf4dffee38aeff
caidev <caidev@localhost>
2020-09-22 00:37:05 -0300

    Proyecto 02 - Calculadora Simple.

--- Proyecto02_Calculadora/src/Logica/CalcuSuma.java
+++ Proyecto02_Calculadora/src/Logica/CalcuSuma.java
@@ -1,6 +1,20 @@
-/**
- * 
+/*
+ * Copyright (C) 2020 caidev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package Logica;
 import GUI.Front;
 
@@ -10,20 +24,15 @@ import GUI.Front;
  */
 public class CalcuSuma {
 
-	public static void Sumar(String num1, String num2) {
-		
-	}
 	/**
-	 * @param args
+	 * @param args default.
 	 */
 	public static void main(String[] args) {
-		 //se instancia el objeto.
+		 // se instancia el objeto.
         Front ventana = new Front();
         ventana.setVisible(true); // la hago visible
-        //para que aparezca en el medio, se pone relative null
-        // ya que no toma nada como referencia para tener uan locacion.
         ventana.setLocationRelativeTo(null);
-
+        // Para que aparezca en el medio, se pone relative null
+        // ya que no toma nada como referencia para tener una locacion.
 	}
-
 }
