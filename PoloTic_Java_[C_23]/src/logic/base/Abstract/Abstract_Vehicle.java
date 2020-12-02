d3c1538c597a5e2bf3a717a23f1b442dc078d135
CaidevOficial <falcone.facu@gmail.com>
2020-12-02 18:39:19 -0300

    Adicion Clase Abstracta Vehiculo.

--- PoloTic_Java_[C_23]/src/logic/base/Abstract/Abstract_Vehicle.java
+++ PoloTic_Java_[C_23]/src/logic/base/Abstract/Abstract_Vehicle.java
@@ -1,6 +1,20 @@
-/**
- * 
+/*
+ * Copyright (C) 2020 FacuFalcone - CaidevOficial
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package logic.base.Abstract;
 
 /**
@@ -17,4 +31,5 @@ public abstract class Abstract_Vehicle {
  	protected String chassisNumber;
  	protected String motor;
  	protected int seatingQuantty;
+    
 }
